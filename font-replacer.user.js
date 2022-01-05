1962d98eed56295393af150c9574dbc2b6a8d10d
Coxxs <3007303+Coxxs@users.noreply.github.com>
2022-01-05 11:33:22 +0800

    Update font-replacer.user.js

--- font-replacer.user.js
+++ font-replacer.user.js
@@ -9,26 +9,25 @@
 // @license     MIT
 // @run-at      document-start
 // ==/UserScript==
-
-function applyCSS(css) {
-  if (!document.head) {
+ 
+function addStyle(css) {
+  const _insertStyle = (css) => {
+    let style = document.createElement("style")
+    style.innerHTML = css
+    document.head.prepend(style)
+  }
+  if (document.head) {
+    _insertStyle(css)
+  } else {
     const observer = new MutationObserver((mutations, observer) => {
       if (!document.head) return
-      addCSS(css)
+      _insertStyle(css)
       observer.disconnect()
     })
     observer.observe(document.documentElement, { childList: true, subtree: true })
-    return
   }
-  addCSS(css)
 }
-
-function addCSS(css) {
-  let style = document.createElement("style")
-  style.innerHTML = css
-  document.head.prepend(style)
-}
-
+ 
 class Replacer {
   constructor() {
     this.css = []
@@ -45,14 +44,23 @@ class Replacer {
     return this.css.join('')
   }
 }
-
+ 
 let replacer = new Replacer()
-
+ 
 /**** Replace rules START *****/
+
+const SourceHanSansSC = [
+  { name: 'Source Han Sans SC' },
+]
+
+const SourceHanSansTC = [
+  { name: 'Source Han Sans TC' },
+]
 
 const HarmonyOSSans = [
   { name: 'HarmonyOS Sans SC' },
 ]
+
 const MiSans = [
   { name: 'MiSans Thin', weight: 100 },
   { name: 'MiSans ExtraLight', weight: 200 },
@@ -63,27 +71,34 @@ const MiSans = [
   { name: 'MiSans Bold', weight: 700 },
   { name: 'MiSans Heavy', weight: 900 },
 ]
-const SourceHanSans = [
-  { name: 'Source Han Sans SC' },
-]
 
-const to = SourceHanSans
-replacer.add("宋体", to)
-replacer.add("SimSun", to)
-replacer.add("新宋体", to)
-replacer.add("NSimSun", to)
-replacer.add("华文中宋", to)
-replacer.add("STZhongsong", to)
-replacer.add("微软雅黑", to)
-replacer.add("Microsoft YaHei", to)
-replacer.add("Microsoft JhengHei", to)
-replacer.add("黑体", to)
-replacer.add("SimHei", to)
-replacer.add("华文细黑", to)
-replacer.add("STXihei", to)
-replacer.add("STHeiTi", to)
-replacer.add("幼圆", to)
+// Simpified Chinese
+const FontSC = SourceHanSansSC
+replacer.add("宋体", FontSC)
+replacer.add("SimSun", FontSC)
+replacer.add("新宋体", FontSC)
+replacer.add("NSimSun", FontSC)
+replacer.add("微软雅黑", FontSC)
+replacer.add("Microsoft YaHei", FontSC)
+replacer.add("黑体", FontSC)
+replacer.add("SimHei", FontSC)
+// replacer.add("华文中宋", FontSC)
+// replacer.add("STZhongsong", FontSC)
+// replacer.add("华文细黑", FontSC)
+// replacer.add("STXihei", FontSC)
+// replacer.add("STHeiTi", FontSC)
+// replacer.add("幼圆", FontSC)
+
+// Traditional Chinese
+const FontTC = SourceHanSansTC
+replacer.add("微軟正黑體", FontTC)
+replacer.add("Microsoft JhengHei", FontTC)
+replacer.add("新細明體", FontTC)
+replacer.add("PMingLiU", FontTC)
+replacer.add("細明體", FontTC)
+replacer.add("MingLiU", FontTC)
+
 
 /**** Replace rules END *****/
-
-applyCSS(replacer.toString())
+ 
+addStyle(replacer.toString())
